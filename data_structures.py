c6c3b0b6cf318222f3a88a4b6e08e4101721a5e2
ugrcnakis <ugrcnakis@gmail.com>
2024-11-15 16:51:40 +0300

    "veri yapıları python dosyası oluşturdum."

---
+++ data_structures.py
@@ -0,0 +1,56 @@
+
+###############################################
+# Veri Yapılarına Giriş 
+##############################################
+
+# Sayılar: integer
+x = 10
+type(x)
+
+
+# Sayılar: float
+x = 10.3
+type(x)
+
+
+# Sayılar: complex
+x = 2j + 1
+type(x)
+
+
+# String
+x = "Hello ai era"
+type(x)
+
+# Boolean
+True
+False
+type(True)
+5 == 4
+3 == 2
+
+type(3 == 2)
+
+
+# Liste
+x = ["btc", "eth", "xrp",5,6]
+type(x)
+
+
+x.append("miuul")
+type(x)
+
+# Sözlük (dictionary)
+x = {"name": "Peter",
+     "Age": 36,
+     "university":"kocaeli"}
+type(x)
+
+
+# Tuple
+x = ("python", "ml", "ds")
+type(x)
+
+# Set
+x = {"python", "ml", "ds"}
+type(x)
